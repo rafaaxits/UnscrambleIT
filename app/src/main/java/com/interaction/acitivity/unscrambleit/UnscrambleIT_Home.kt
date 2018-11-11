59366b7de6c86c97abe5b69987cb7c91adaf206d
rafaaxits <rafaaxits@gmail.com>
2018-11-11 13:48:04 -0300

    Starts the project / Adds the home screen of the app

---
+++ app/src/main/java/com/interaction/acitivity/unscrambleit/UnscrambleIT_Home.kt
@@ -0,0 +1,12 @@
+package com.interaction.acitivity.unscrambleit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UnscrambleIT_Home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unscramble_it__home)
+    }
+}
