e1e2b2ce9ea552c1a4bb2e5eae2ba0f09c7fb263
Nycolas Lacerda <nycolas.lacerda@hotmail.com>
2018-11-19 19:18:51 -0200

    Add state
    Add state in beginner level 1

--- app/src/main/java/com/interaction/acitivity/unscrambleit/UnscrambleIT_beginner_level1.kt
+++ app/src/main/java/com/interaction/acitivity/unscrambleit/UnscrambleIT_beginner_level1.kt
@@ -5,11 +5,16 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_unscramble_it_beginner_level1.*
 
 class UnscrambleIT_beginner_level1 : AppCompatActivity() {
-
+  val KEY_TEXT_VALUE = "";
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_unscramble_it_beginner_level1)
     txtViewAnswer.setText("");
+
+    if(savedInstanceState != null){
+      txtViewAnswer.setText(savedInstanceState.getString(KEY_TEXT_VALUE))
+    }
+
     btnThe.setOnClickListener(){
       txtViewAnswer.setText(txtViewAnswer.text.toString() +" "+ btnThe.text.toString() +" ")
     }
@@ -25,8 +30,13 @@ class UnscrambleIT_beginner_level1 : AppCompatActivity() {
     btnTable.setOnClickListener(){
       txtViewAnswer.setText(txtViewAnswer.text.toString() +" "+ btnTable.text.toString()+" ")
     }
-    
 
 
+
+
+  }
+  override fun onSaveInstanceState(outState: Bundle) {
+    outState.putCharSequence(KEY_TEXT_VALUE, txtViewAnswer.text);
+    super.onSaveInstanceState(outState)
   }
 }
